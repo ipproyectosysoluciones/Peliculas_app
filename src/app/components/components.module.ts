cc71ec9cdf15b872384c0c1bff70a9d26a7c5809
Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>
2022-11-29 11:47:57 -0500

    Poster Pipe
    Signed-off-by: Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
 import { RatingModule } from 'ng-starrating';
+import { PipesModule } from './../pipes/pipes.module';
 
 
 
@@ -20,9 +21,10 @@ import { RatingModule } from 'ng-starrating';
     PeliculasPosterGridComponent
   ],
   imports: [
-  CommonModule,
-  RouterModule,
-  RatingModule
+    CommonModule,
+    RouterModule,
+    RatingModule,
+    PipesModule
   ]
 })
 export class ComponentsModule { }
