3193c4e95ab3a19c5fc39834c7ca721e533afb3a
Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>
2022-11-29 17:04:25 -0500

    Diseño pantalla detalle Película
    Signed-off-by: Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -1,5 +1,7 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MovieResponse } from '../../interfaces/movie-response';
 import { PeliculasService } from '../../services/peliculas.service';
 
 @Component({
@@ -9,17 +11,33 @@ import { PeliculasService } from '../../services/peliculas.service';
 })
 export class PeliculaComponent implements OnInit {
 
+  public movie: MovieResponse;
+
+  /**
+   * @name constructor PeliculaComponent
+   * @param activateRoute
+   * @param peliculasService
+   * @param location
+   */
   constructor ( private activateRoute: ActivatedRoute,
-                private peliculasService: PeliculasService) { }
+                private peliculasService: PeliculasService,
+                private location: Location) { }
 
   ngOnInit (): void {
     const id = this.activateRoute.snapshot.params[ 'id' ];
 
     this.peliculasService.getPeliculaDetalle( id ).subscribe( movie => {
       console.log( movie );
+      this.movie = movie;
     } )
+    // console.log(id);
+  }
 
-    console.log(id);
+  /**
+   * @name onRegresar PeliculaComponent
+   */
+  onRegresar () {
+    this.location.back();
   }
 
 }
